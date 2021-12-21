bac8de75a266ebba3a2f0b2f2a49edeeb86fe978
John <johnnonso090@gmail.com>
2021-12-21 15:26:18 +0100

    Fixed some cors issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,17 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+
+    if Rails.env.development?
+        origins 'localhost:3000'
+    else
+        origins  'https://peoplefav.herokuapp.com/'
+    end
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
