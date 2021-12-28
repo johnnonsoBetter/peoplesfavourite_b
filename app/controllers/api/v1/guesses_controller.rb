c94d638c37f6f3035c4f441f3638a28f2677bb9e
John <johnnonso090@gmail.com>
2021-12-28 09:43:19 +0100

    fixed creating guesses scope bug

--- app/controllers/api/v1/guesses_controller.rb
+++ app/controllers/api/v1/guesses_controller.rb
@@ -1,12 +1,11 @@
 class Api::V1::GuessesController < ApplicationController 
     before_action :authenticate_api_v1_user!, only: :show
 
-    def show 
-
-
-        @guess = Guess.create_with(thing_type_id: params[:thing_type_id], user: current_api_v1_user).find_or_create_by(thing_type_id: params[:thing_type_id])
-        
-        
+    def show
+         
+        @user = current_api_v1_user
+        @guess =   @user.guesses.create_with(thing_type_id: params[:thing_type_id], user: @user).find_or_create_by(thing_type_id: params[:thing_type_id])
+    
         render 'api/v1/guesses/show.json.jbuilder'
     end
 end
